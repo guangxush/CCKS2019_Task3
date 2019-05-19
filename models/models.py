e0f743c601f63d6bb74dcb84b91e312d6ff2b44e
guangxush <guangxush@163.com>
2019-05-19 15:51:54 +0800

    Keras

--- models/models.py
+++ models/models.py
@@ -377,14 +377,14 @@ class Models(object):
     # 多任务模型加入距离信息
     def cnn_multi_dis(self):
         sentence = Input(shape=(self.config.max_len,), dtype='int32', name='sent_base')
-        dis1 = Input(shape=(self.config.max_len, 1), dtype='int32', name='disinfos1')
-        dis2 = Input(shape=(self.config.max_len, 1), dtype='int32', name='disinfos2')
+        dis1 = Input(shape=(self.config.max_len, 1), dtype='float32', name='disinfos1')
+        dis2 = Input(shape=(self.config.max_len, 1), dtype='float32', name='disinfos2')
         weights = np.load(os.path.join(self.config.embedding_path, self.config.embedding_file))
         embedding_layer = Embedding(input_dim=weights.shape[0],
                                     output_dim=weights.shape[-1],
                                     weights=[weights], name='embedding_layer', trainable=True)
         sent_embedding = embedding_layer(sentence)
-        all_input = K.concatenate([sent_embedding, dis1, dis2], axis=1)
+        all_input = concatenate([sent_embedding, dis1, dis2], axis=2)
         filter_length = 3
         conv_layer = Conv1D(filters=100, kernel_size=filter_length, padding='valid', strides=1, activation='relu')
         sent_c = conv_layer(all_input)
@@ -462,12 +462,18 @@ class Models(object):
 
     def fit_multi_dis(self, x_train, x_train_dis1, x_train_dis2, y_train, y_train2, x_valid, x_valid_dis1, x_valid_dis2, y_valid, y_valid2):
         x_train = self.pad(x_train)
+        x_train_dis1 = np.asarray(x_train_dis1)
+        x_train_dis2 = np.asarray(x_train_dis2)
+
         x_train_dis1 = x_train_dis1.reshape(len(x_train_dis1), self.config.max_len, 1)
         x_train_dis2 = x_train_dis2.reshape(len(x_train_dis2), self.config.max_len, 1)
         x_train_dis1 = self.pad(x_train_dis1)
         x_train_dis2 = self.pad(x_train_dis2)
 
         x_valid = self.pad(x_valid)
+        x_valid_dis1 = np.asarray(x_valid_dis1)
+        x_valid_dis2 = np.asarray(x_valid_dis2)
+
         x_valid_dis1 = x_train_dis1.reshape(len(x_valid_dis1), 1)
         x_valid_dis1 = self.pad(x_valid_dis1)
         x_valid_dis2 = x_train_dis1.reshape(len(x_valid_dis2), 1)
