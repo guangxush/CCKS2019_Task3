f5f169a644bbbc0347942408c13458860909a4fd
guangxush <guangxush@163.com>
2019-06-07 21:04:10 +0800

    base

--- main.py
+++ main.py
@@ -110,8 +110,56 @@ def model_select(model_name, x_train, x_train_dis1, x_train_dis2, y_train, x_val
         # 固定最大长度，多余的截取掉，不足的用0填充
         config.max_len = config.max_len_word
         config.vocab_len = config.vocab_len_word
-    elif level == 'bag':
-        config.max_len = config.max_bag_len_word
+    else:
+        config.max_len = config.max_len_char
+        config.vocab_len = config.vocab_len_char
+    config.exp_name = model_name
+
+    # 训练的模型保存成文件的形式
+    if not os.path.exists(config.checkpoint_dir):
+        os.makedirs(config.checkpoint_dir)
+
+    # 获取词向量文件
+    config.embedding_file += 'embeddings'
+    # 载入配置文件
+    model = Models.Models(config)
+
+    # 模型训练
+    print('Create the ' + model_name + ' model...')
+    if model_name == 'cnn_base':
+        model.cnn_base()
+    elif model_name == 'bilstm_base':
+        model.bilstm_base()
+    elif model_name == 'cnn':
+        model.cnn()
+    elif model_name == 'lstm_attention':
+        model.lstm_attention()
+    elif model_name == 'mlp_bag':
+        model.mlp()
+    else:
+        return
+
+    if overwrite or not os.path.exists(os.path.join(config.checkpoint_dir, '%s.hdf5' % model_name)):
+        print('Start training the ' + model_name + ' model...')
+        model.fit(x_train, x_train_dis1, x_train_dis2, y_train, x_valid, x_valid_dis1, x_valid_dis2, y_valid)
+    model.load_weight()
+    print('Start evaluate the ' + model_name + ' model...')
+    y_valid_pred = model.predict(x_valid, x_valid_dis1, x_valid_dis2)
+    y_test_pred = model.predict(x_test, x_test_dis1, x_test_dis2)
+    model.evaluate(model_name, y_valid_pred, y_valid)
+    print('Start generate the ' + model_name + ' model...')
+
+    return y_test_pred
+
+
+# 创建单一任务模型
+def model_bag_select(model_name, x_train, x_train_dis1, x_train_dis2, y_train, x_valid, x_valid_dis1, x_valid_dis2, y_valid,
+                 x_test, x_test_dis1, x_test_dis2, level, overwrite=False):
+    config = Config()
+    config.level = level
+    if level == 'word':
+        # 固定最大长度，多余的截取掉，不足的用0填充
+        config.max_len = config.max_len_word
         config.vocab_len = config.vocab_len_word
     else:
         config.max_len = config.max_len_char
@@ -280,9 +328,9 @@ if __name__ == '__main__':
                          test_file='./data/bag_test.txt', flag='train')
 
         # cnn base model
-        y_test_pred = model_select('cnn_base_bag', x_train, x_train_dis1, x_train_dis2, y_train, x_valid, x_valid_dis1,
-                                   x_valid_dis2, y_valid,
-                                   x_test, x_test_dis1, x_test_dis2, level, overwrite=overwrite)
+        y_test_pred = model_bag_select('cnn_base_bag', x_train, x_train_dis1, x_train_dis2, y_train, x_valid, x_valid_dis1,
+                                       x_valid_dis2, y_valid,
+                                       x_test, x_test_dis1, x_test_dis2, level, overwrite=overwrite)
 
         generate_result(ids, y_test_pred)
 
